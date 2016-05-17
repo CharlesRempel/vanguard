54fe72cd0cc33af96d7a79afe8f8ba03c8b41bb3
CharlesRempel <pikachu.x.ghost@gmail.com>
2016-05-17 11:06:40 -0400

    Clean Up
    Adding Comments to CSS
    JS file finished functionality on tabs

--- js/main.js
+++ js/main.js
@@ -3,8 +3,41 @@
 });*/
 
 $(function() {
-    $('#cowboy section h2#hidden').click(function(e) {
+    $('#cowboy section#work h2#hidden').click(function(e) {
         e.preventDefault();
         $('#caseStudy').toggleClass('display');
+        $('#start').removeClass('display');
+        $('#staff').removeClass('display');
+        $('#contact').removeClass('display');
+    });
+});
+
+$(function() {
+    $('#cowboy section#process h2#hidden').click(function(e) {
+        e.preventDefault();
+        $('#start').toggleClass('display');
+        $('#caseStudy').removeClass('display');
+        $('#staff').removeClass('display');
+        $('#contact').removeClass('display');
+    });
+});
+
+$(function() {
+    $('#cowboy section#team h2#hidden').click(function(e) {
+        e.preventDefault();
+        $('#staff').toggleClass('display');
+        $('#caseStudy').removeClass('display');
+        $('#start').removeClass('display');
+        $('#contact').removeClass('display');
+    });
+});
+
+$(function() {
+    $('#cowboy section#hire h2#hidden').click(function(e) {
+        e.preventDefault();
+        $('#contact').toggleClass('display');
+        $('#caseStudy').removeClass('display');
+        $('#start').removeClass('display');
+        $('#staff').removeClass('display');
     });
 });

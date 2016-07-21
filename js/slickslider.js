054f45b55e1b98095ea52a2ac6f065c9863525ec
CharlesRempel <pikachu.x.ghost@gmail.com>
2016-07-21 18:37:18 -0400

    New Changes
    Adding content

---
+++ js/slickslider.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.slickProcess').slick({
+      mobileFirst: true,
+      cssEase: 'ease',
+      accessibility: true,
+      centerPadding: '50px',
+      initialSlide: 0,
+      swipe: true,
+      touchMove: true,
+      draggable: true,
+    });
+  });
